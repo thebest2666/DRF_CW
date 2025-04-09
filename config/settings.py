6d9f450e18def77765d7e0f45f36081071c384fd
alexander anciferov <sania.antsiferoff@yandex.ru>
2025-04-10 00:09:59 +0800

    deploy

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "drf_cw8",
+        "NAME": "postgres",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "db",
